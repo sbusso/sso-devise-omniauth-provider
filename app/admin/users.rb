094a12eb632226ca8167dc06db37c969206a67f4
Stéphane Busso <stephane.busso@gmail.com>
2012-10-28 11:27:44 +0700

    config admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :email
+    column :first_name
+    column :last_name
+    column :last_sign_in_at
+    column :sign_in_count
+    column :created_at
+    default_actions
+  end
 end
