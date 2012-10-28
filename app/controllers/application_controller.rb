de00aa398d16b656b4996a8e090abdc503f9ae67
Stéphane Busso <stephane.busso@gmail.com>
2012-10-28 12:54:30 +0700

    fix layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # layout :layout_by_resource
+  layout :layout_by_resource
 
-  # protected
+  protected
 
-  # def layout_by_resource
-  #   if devise_controller?
-  #     "sign_up"
-  #   else
-  #     "application"
-  #   end
-  # end
+  def layout_by_resource
+    if devise_controller?
+      "sign_up"
+    else
+      "application"
+    end
+  end
 end
